b29402ed1b548a6eaba1e7137cd9dba333348f1d
bread1q <osnova326@gmail.com>
2025-12-03 17:54:05 +0500

    Изменен вид вывода

--- laba.cpp
+++ laba.cpp
@@ -7,208 +7,119 @@ class Point {
     private:
         double x_ = 0;
         double y_ = 0;
-
     public:
-        // методы
-        double GetX();
-        double GetY();
-        void SetX();
-        void SetY();
+        double GetX() { return x_; };
+        double GetY() { return y_; };
+        void SetX(double x) { x_ = x; };
+        void SetY(double y) { y_ = y; };
         void Print();
 
-        // конструкторы
         Point();
         Point(double x, double y);
         Point(const Point& other);
 
-        // деструктор
         ~Point();
 };
 
-double Point::GetX() { return x_;}
-double Point::GetY() { return y_;}
-void Point::SetX() {
-    int x;
-    cout << "Введите X: ";
-    cin >> x;
-    cout << endl;
-    x_ = x;
-}
-void Point::SetY() {
-    int y;
-    cout << "Введите Y: ";
-    cin >> y;
-    cout << endl;
-    y_ = y;
+void Point::Print() { cout << "Point::Print(): X = " << x_ << ", Y = " << y_; }
+
+Point::Point() : x_(0), y_(0) { cout << "Point: Конструктор по умолчанию" << endl; }
+
+Point::Point(double x, double y) : x_(x), y_(y) { 
+    cout << "Point: Конструктор с параметрами (" << x << ", " << y << ")" << endl;
 }
 
-void Point::Print() {
-    cout << "\nВызван метод Print()\n";
-    cout << "Координата X: " << GetX() << ", координата Y: " << GetY() << endl;
-}
+Point::Point(const Point& other) : x_(other.x_), y_(other.y_) { cout << "Point: Конструктор копирования" << endl; }
 
-Point::Point() {
-    x_ = 0;
-    y_ = 0;
-    cout << "Вызван конструктор Point()\n";
-    cout << "Объект класса Point создан.\n";
-}
-
-Point::Point(double x, double y) {
-    x_ = x;
-    y_ = y;
-    cout << "Вызван конструктор Point(double x, double y)\n";
-    cout << "Объект класса Point создан\n";
-}
-
-Point::Point(const Point& other) {
-    x_ = other.x_;
-    y_ = other.y_;
-    cout << "Вызван конструктор Point(const Point& other)\n";
-    cout << "Объект класса Point создан.\n";
-}
-
-Point::~Point() {
-    cout << "Вызван деструктор ~Point(). Объект удалён." << endl;
-}
+Point::~Point() { cout << "Point: Деструктор (" << x_ << ", " << y_ << ")" << endl; }
 
 class ColoredPoint : public Point {
     private: 
         string color_;
-
     public:
         ColoredPoint();
         ColoredPoint(double x, double y, const string& color);
         ColoredPoint(const ColoredPoint& other);
+
         ~ColoredPoint();
 
-        string GetColor();
-        void SetColor(const string& color);
+        string GetColor() { return color_; };
+        void SetColor(const string& color) { color_ = color; };
         void Print();
 };
 
-ColoredPoint::ColoredPoint() : Point(), color_("black") {
-    cout << "\nВызван конструктор ColoredPoint()\n";
-    cout << "Объект класса ColoredPoint создан.\n";
-}
+ColoredPoint::ColoredPoint() : Point(), color_("black") { cout << "ColoredPoint: Конструктор по умолчанию" << endl; }
 
 ColoredPoint::ColoredPoint(double x, double y, const string& color) : Point(x, y), color_(color) {
-    cout << "\nВызван констуктор ColoredPoint(double x, double y, const string& color)\n";
-    cout << "Объект класса ColoredPoint создан.\n";
+    cout << "ColoredPoint: Конструктор с параметрами (" << color << ")" << endl;
 }
 
 ColoredPoint::ColoredPoint(const ColoredPoint& other) : Point(other), color_(other.color_) {
-    cout << "\nВызван констуктор ColoredPoint(const ColoredPoint& other)\n";
-    cout << "Объект класса ColoredPoint создан.\n";
+    cout << "ColoredPoint: Конструктор копирования" << endl;
 }
 
-ColoredPoint::~ColoredPoint() {
-    cout << "Вызван деструктор ~ColoredPoint(). Объект удалён." << endl;
-}
-
-string ColoredPoint::GetColor() { return color_; }
-
-void ColoredPoint::SetColor(const string& color) { color_ = color; }
+ColoredPoint::~ColoredPoint() { cout << "ColoredPoint: Деструктор (" << color_  << ")" << endl; }
 
 void ColoredPoint::Print() {
+    cout << "ColoredPoint::Print(): ";
     Point::Print();
-    cout << "Color: " << GetColor() << endl;
+    cout << ", color = " << GetColor() << endl;
 }
 
 int main() {
-    cout << "\nСоздаем динамический объект Point по умолчанию....." << endl;
-    cout << "===================================================" << endl;
+    cout << "=============== Часть 1 ===============\n";
+
+    cout << "\n1. Создание динамических объектов Point.\n" << endl;
     Point *pa = new Point;
-    pa->Print();
-
-    cout << "\n\n";
-
-    cout << "\nСоздаем динамический объект Point....." << endl;
-    cout << "======================================" << endl;
     Point *pb = new Point(4.0, 10.0);
-    pb->Print();
-
-    cout << "\n\n";
-
-    cout << "\nКопируем динамический объект Point в другой объект Point....." << endl;
-    cout << "=============================================================" << endl;
     Point *pc = new Point(*pb);
-    pc->Print();
 
-    cout << "\nУдаляем последний созданный динамический объект....." << endl;
-    cout << "===========================================================" << endl;
+    cout << "\n2. Вызов методов.\n" << endl;
+    pa->Print(); cout << endl;
+    pb->Print(); cout << endl;
+    pc->Print(); cout << endl;
+
+    cout << "\n3. Удаление динамических объектов Point.\n" << endl;
     delete pa;
+    delete pb;
+    delete pc;
 
-    cout << "\n\n";
-
-    cout << "\nСоздаем объект Point по умолчанию....." << endl;
-    cout << "======================================" << endl;
+    cout << "\n4. Создание статических объектов Point.\n" << endl;
     Point a;
-    a.Print();
-
-    cout << "\n\n";
-
-    cout << "\nСоздаем объект Point....." << endl;
-    cout << "=========================" << endl;
-    Point b(2.5, 3.5);
-    b.Print();
-
-    cout << "\n\n";
-
-    cout << "\nКопируем объект Point в другой объект Point....." << endl;
-    cout << "================================================" << endl;
+    Point b(5.0, 8.5);
     Point c(b);
-    c.Print();
 
-    cout << "\n\n";
+    cout << "\n5. Вызов методов.\n" << endl;
+    a.Print(); cout << endl;
+    b.Print(); cout << endl;
+    c.Print(); cout << endl;
 
-    cout << "\nСоздаем динамический объект ColoredPoint по умолчанию....." << endl;
-    cout << "==========================================================" << endl;
+    cout << "\n\n=============== Часть 2 ===============\n";
+
+    cout << "\n6. Создание динамических объектов ColoredPoint.\n" << endl;
     ColoredPoint *cp1 = new ColoredPoint();
-    cp1->Print();
-
-    cout << "\n\n";
-
-    cout << "\nСоздаем динамический объект ColoredPoint....." << endl;
-    cout << "=============================================" << endl;
-    ColoredPoint *cp2 = new ColoredPoint(3.0, 8.5, "blue");
-    cp2->Print();
-
-    cout << "\n\n";
-
-    cout << "\nКопируем динамический объект ColoredPoint в другой объект ColoredPoint....." << endl;
-    cout << "===========================================================================" << endl;
+    ColoredPoint *cp2 = new ColoredPoint(3.0, 6.0, "blue");
     ColoredPoint *cp3 = new ColoredPoint(*cp1);
+
+    cout << "\n7. Вызов методов.\n" << endl;
+    cp1->Print();
+    cp2->Print();
     cp3->Print();
 
-    cout << "\n\n";
-
-    cout << "\nУдаляем созданные динамические объекты....." << endl;
-    cout << "===========================================" << endl;
+    cout << "\n8. Удаление динамических объектов ColoredPoint.\n" << endl;
     delete cp1;
     delete cp2;
     delete cp3;
 
-    cout << "\n\n";
+    cout << "\n9. Создание статических объектов ColoredPoint.\n" << endl;
+    ColoredPoint cpa;
+    ColoredPoint cpb(5.0, 8.5, "white");
+    ColoredPoint cpc(cpb);
 
-    cout << "\nСоздаем объект ColoredPoint по умолчанию....." << endl;
-    cout << "======================================" << endl;
-    ColoredPoint cp_1;
-    cp_1.Print();
-
-    cout << "\n\n";
-
-    cout << "\nСоздаем объект ColoredPoint....." << endl;
-    cout << "================================" << endl;
-    ColoredPoint cp_2(5.0, 2.0, "white");
-    cp_2.Print();
-
-    cout << "\n\n";
-
-    cout << "\nКопируем объект ColoredPoint в другой объект ColoredPoint....." << endl;
-    cout << "==============================================================" << endl;
-    ColoredPoint cp_3(cp_2);
-    cp_3.Print();
+    cout << "\n10. Вызов методов.\n" << endl;
+    cpa.Print();
+    cpb.Print();
+    cpc.Print();
 
     cout << "\n\n";
 
